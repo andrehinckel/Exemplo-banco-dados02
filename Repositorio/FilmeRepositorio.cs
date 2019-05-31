5e8f45f783cf505bde9a6db62a8f176a0380e0d5
andrehinckel <andrehinckel@gmail.com>
2019-05-31 14:36:33 -0300

    Desenvolvimento dos métodos para obter o filme pelo id inserir o registro do filme no bd.

--- Repositorio/FilmeRepositorio.cs
+++ Repositorio/FilmeRepositorio.cs
@@ -12,6 +12,7 @@ namespace Repositorio
     public class FilmeRepositorio
     {
         string CadeiaConexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\65970\Documents\ExemploBancoDados02.mdf;Integrated Security=True;Connect Timeout=30";
+
         /*
          * Método que irá retornar os dados dos filmes
          * filmes(List<Filme>) da tabela de filmes
@@ -43,6 +44,7 @@ namespace Repositorio
             // Cria uma lista para adicionar os filmes do bd
             List<Filme> filmes = new List<Filme>();
 
+
             // Percorre todos os registros lidos do bd
             for (int i = 0; i < table.Rows.Count; i++)
             {
@@ -63,6 +65,56 @@ namespace Repositorio
             connection.Close();
             // retorna a lista de filmes
             return filmes;
+        }
+
+        public Filme ObterPeloId(int id)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = CadeiaConexao;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM filmes WHERE id = @ID";
+            command.Parameters.AddWithValue("ID", id);
+
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(command.ExecuteReader());
+            if (dataTable.Rows.Count == 1)
+            {
+                DataRow row = dataTable.Rows[0];
+                Filme filme = new Filme();
+                filme.Id = Convert.ToInt32(row["id"]);
+                filme.Nome = row["nome"].ToString();
+                filme.Categoria = row["categoria"].ToString();
+                filme.Curtiu = Convert.ToBoolean(row["curtiu"]);
+                filme.Duracao = Convert.ToDateTime(row["duracao"]);
+                filme.Avaliacao = Convert.ToDecimal(row["avaliacao"]);
+                filme.TemSequencia = Convert.ToBoolean(row["tem_sequencia"]);
+                connection.Close();
+                return filme;
+            }
+            return null;
+        }
+
+        public void Inserir(Filme filme)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = CadeiaConexao;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "INSERT INTO filmes (nome, categoria, curtiu, duracao, avaliacao, tem_sequencia) VALUES (@NOME, @CATEGORIA, @CURTIU, @DURACAO, @AVALIACAO, @TEM_SEQUENCIA)";
+            command.Parameters.AddWithValue("@NOME", filme.Nome);
+            command.Parameters.AddWithValue("CATEGORIA", filme.Categoria);
+            command.Parameters.AddWithValue("@CURTIU", filme.Curtiu);
+            command.Parameters.AddWithValue("@DURACAO", filme.Duracao);
+            command.Parameters.AddWithValue("@AVALIACAO", filme.Avaliacao);
+            command.Parameters.AddWithValue("@TEM_SEQUENCIA", filme.TemSequencia);
+            command.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
